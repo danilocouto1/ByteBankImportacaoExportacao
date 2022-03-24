25b57b9c874b0446bde2d848ab6b9c87e4a22b48
Danilo <dansantosalfa@gmail.com>
2022-03-24 13:34:52 -0300

    Encerramento do curso

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -12,7 +12,42 @@ namespace ByteBankImportacaoExportacao
     {
         static void Main(string[] args)
         {
+            File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText");
+
+            Console.WriteLine("Arquivo escrevendoComAClasseFile criado!");
+
+            var bytesArquivo = File.ReadAllBytes("contas.txt");
+            Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length} bytes");
+
+
+
+
+            var linhas = File.ReadAllLines("contas.txt");
+            Console.WriteLine(linhas.Length);
+
+            foreach (var linha in linhas)
+            {
+                Console.WriteLine(linha);
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+            Console.WriteLine("Digite o seu nome:");
+            var nome = Console.ReadLine();
+
+            Console.WriteLine(nome);
+
+
             testeEntrada();
+
+            Console.WriteLine("Aplicação finalizada. . .");
+
             Console.ReadLine();
         }
     }
