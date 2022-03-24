2b32fced3d77067cf07fbb430416312ce8119443
Danilo <dansantosalfa@gmail.com>
2022-03-24 11:57:37 -0300

    Aprendendo uso do Flush e do BynariReader

--- ByteBankImportacaoExportacao/3-CriandoArquivo.cs
+++ ByteBankImportacaoExportacao/3-CriandoArquivo.cs
@@ -30,6 +30,33 @@ namespace ByteBankImportacaoExportacao
                 write.Write(stringzada);
             }
         }
+        static void TestaEscrita()
+        {
+            var caminhoArquivo = "teste.txt";
 
+            using (var fluxoDeArquivo = new FileStream(caminhoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoDeArquivo))
+            {
+                for (int i = 0; i < 100000000; i++)
+                {
+                    escritor.WriteLine($"Linha {i}");
+                    Console.WriteLine($"Linha {i} foi escrita no arquivo. Tecle enter p adicionar mais uma!");
+                    Console.ReadLine();
+                }
+            }
+        }
+        static void LeituraBinaria()
+        {
+            using (var fs = new FileStream("contaCorrente.txt", FileMode.Open))
+            using (var leitor = new BinaryReader(fs))
+            {
+                var agencia = leitor.ReadInt32();
+                var numeroConta = leitor.ReadInt32();
+                var saldo = leitor.ReadDouble();
+                var titular = leitor.ReadString();
+
+                Console.WriteLine($"{agencia}/{numeroConta} {titular} {saldo}");
+            }
+        }
     }
 }
